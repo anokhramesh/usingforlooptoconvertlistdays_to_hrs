765ace4f33f0bc0ec3668acf1b7bc9fce166f92d
anokhramesh <67797819+anokhramesh@users.noreply.github.com>
2021-07-04 21:34:35 +0400

    Update using_forloop_toConvert_multipledays(list)_to_hrs.py

--- using_forloop_toConvert_multipledays(list)_to_hrs.py
+++ using_forloop_toConvert_multipledays(list)_to_hrs.py
@@ -1,11 +1,12 @@
+# In this program we will convert a list of days to hours using for loop and split function in python.
 calculation_to_units =24
 name_of_unit = "Hours"
 
-def days_to_units(num_of_days):
+def days_to_units(num_of_days):# created a function name -days_to_units with an argument of num_of_days.
     return f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}"
-def validate_and_execute():
+def validate_and_execute():#function for validation if user input is int data type,then only calculate conversion.
     try:
-        user_input_number =int(num_of_days_elements)
+        user_input_number =int(num_of_days_elements)#type casting the usre input to int data type.
         if user_input_number >0:
             calculated_value = days_to_units(user_input_number)
             print(calculated_value)
@@ -19,5 +20,5 @@ def validate_and_execute():
 user_input =""
 while user_input != "exit":
     user_input =input("Enter number of Days for convert to Hours\n")
-    for num_of_days_elements in user_input.split(","):
+    for num_of_days_elements in user_input.split(","):#using split function to split the user input lits with comas.
         validate_and_execute()
